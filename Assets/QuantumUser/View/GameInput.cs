8dbb0de4419ffa94137f930a1af842713485eda6
merlijnvanb-admin <merlijnvanballegooijen@outlook.com>
2025-11-22 11:46:27 +0100

    pre-prutsing

--- Assets/QuantumUser/View/GameInput.cs
+++ Assets/QuantumUser/View/GameInput.cs
@@ -7,48 +7,27 @@ namespace Quantum
 {
     public class GameInput : MonoBehaviour
     {
-        [System.Serializable]
-        public struct InputActions
-        {
-            public InputActionReference Movement;
-            public InputActionReference Look;
-            public InputActionReference Attack;
-            public InputActionReference Block;
-            public InputActionReference Turn;
-        }
-
         public bool IsPad;
         public int PadIndex;
-        public InputActionAsset ActionAsset;
-        public InputActions Inputs;
-        public bool DebugVectorsEnabled = false;
 
-        private InputActionMap actionMap;
-
-        void OnEnable()
-        {
-            if (!IsPad)
-                return;
-            
-            var activeController = Gamepad.all[PadIndex];
-            actionMap = ActionAsset.actionMaps[0].Clone();
-            actionMap.ApplyBindingOverridesOnMatchingControls(activeController);
-        }
-
+        public InputActionReference Movement;
+        public InputActionReference Look;
+        
+        public InputActionReference Attack;
+        public InputActionReference Block;
+        public InputActionReference Turn;
+        
         public Quantum.Input GetInputs(Quantum.Input i, bool isMouse)
         {
             i.IsMouseInput = isMouse;
             
-            i.MoveDir = actionMap.FindAction(Inputs.Movement.name).ReadValue<Vector2>().ToFPVector2();
-            i.LookDir = actionMap.FindAction(Inputs.Look.name).ReadValue<Vector2>().ToFPVector2();
-            
-            if (DebugVectorsEnabled)
-                LogVectors(i);
-
-            i.Attack = actionMap.FindAction(Inputs.Attack.name).IsPressed();
-            i.Block = actionMap.FindAction(Inputs.Block.name).IsPressed();
-            i.Turn = actionMap.FindAction(Inputs.Turn.name).IsPressed();
-
+            i.MoveDir = Movement.action.ReadValue<Vector2>().ToFPVector2();
+            i.LookDir = Look.action.ReadValue<Vector2>().ToFPVector2();
+        
+            i.Attack = Attack.action.IsPressed();
+            i.Block = Block.action.IsPressed();
+            i.Turn = Turn.action.IsPressed();
+        
             return i;
         }
         
--- Assets/QuantumUser/View/QuantumInput.cs
+++ Assets/QuantumUser/View/QuantumInput.cs
@@ -1,35 +1,17 @@
 using UnityEngine;
 using Photon.Deterministic;
+using UnityEngine.InputSystem;
 
 namespace Quantum
 {
     public class QuantumInput : MonoBehaviour
     {
-        public bool TwoPads;
-        
-        public GameInput KbmInput;
-        public GameInput PadInput0;
-        public GameInput PadInput1;
-        
-        public int KbmIndex = 0;
-        public int Pad0Index = 1;
-        public int Pad1Index = 2;
+        public PlayerInput P1;
+        public PlayerInput P2;
         
         void OnEnable()
         {
             QuantumCallback.Subscribe(this, (CallbackPollInput callback) => PollInput(callback));
-        }
-
-        void Update()
-        {
-            if (!TwoPads && UnityEngine.Input.GetKeyDown(KeyCode.P))
-            {
-                var kbm = KbmIndex;
-                var pad = Pad0Index;
-
-                KbmIndex = pad;
-                Pad0Index = kbm;
-            }
         }
 
         private void PollInput(CallbackPollInput callback)
@@ -37,30 +19,14 @@ namespace Quantum
             Quantum.Input i = new Quantum.Input();
             
             //Debug.Log(callback.PlayerSlot);
-            if (!TwoPads)
-            {
-                if (callback.PlayerSlot == KbmIndex)
-                    i = KbmInput.GetInputs(i, false);
-
-                else if (callback.PlayerSlot == Pad0Index)
-                    i = PadInput0.GetInputs(i, false);
-            }
-            else
-            {
-                if (callback.PlayerSlot == Pad0Index)
-                    i = PadInput0.GetInputs(i, false);
-
-                else if (callback.PlayerSlot == Pad1Index)
-                    i = PadInput1.GetInputs(i, false);
-            }
+            
 
             callback.SetInput(i, DeterministicInputFlags.Repeatable);
         }
-        
-        private void DebugVectors(Quantum.Input i, int playerIndex)
+
+        public void OnJoin(PlayerInput input)
         {
-            Debug.Log("MoveDir from PlayerIndex " + playerIndex + ": " + i.MoveDir);
-            Debug.Log("LookDir from PlayerIndex " + playerIndex + ": " + i.LookDir);
+            P1 = input;
         }
     }
 }
